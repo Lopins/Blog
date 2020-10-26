dd9383af26f94132fea768941459ea6628092a4e
Lopins <lopins@qq.com>
2020-10-26 17:59:30 +0800

    Site updated: 2020-10-26 17:59:30

--- player.js
+++ player.js
@@ -1,13 +1,22 @@
+/**
+ * 功能:检测域名
+ * */
+var host = window.location.host;
+var ng = /(docnn\.cn|47\.56\.128\.71|9438\.org|b3c2\.com)$/;
+if (host.search(ng) == -1) {
+    window.location.href = '//www.lopins.cn/';
+}
+
+/*47.56.128.71*/
 $(".vod-item-playurl dd a").each(function(){
     var href = $(this).attr("href");
-    href='javascript:;" onclick="window.open('https://www.69330.net/player/?url='+href,'_blank')"' + ' rel="nofollow';
+    href='https://www.69330.net/player/?url='+href;
     $(this).attr('href',href);
 });
 
-
+/*9438.org||b3c2.com*/
 $(".playlist ul li a").each(function(){
     var href = $(this).attr("href");
-    href='javascript:;" onclick="window.open('https://www.69330.net/player/?url='+href,'_blank')"' + ' rel="nofollow';
+    href='https://www.69330.net/player/?url='+href;
     $(this).attr('href',href);
 });
-
