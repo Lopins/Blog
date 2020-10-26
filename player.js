df5f6cce8ee6f29ccfddb25df13bb532d6401a1d
Lopins <lopins@qq.com>
2020-10-26 17:49:01 +0800

    Site updated: 2020-10-26 17:49:01

--- player.js
+++ player.js
@@ -1,12 +1,13 @@
 $(".vod-item-playurl dd a").each(function(){
     var href = $(this).attr("href");
-    href='javascript:;" onclick="https://www.69330.net/player/?url='+href;
+    href='javascript:;" onclick="window.open('https://www.69330.net/player/?url='+href,'_blank')"' + ' rel="nofollow';
     $(this).attr('href',href);
 });
 
 
 $(".playlist ul li a").each(function(){
     var href = $(this).attr("href");
-    href='javascript:;" onclick="https://www.69330.net/player/?url='+href;
+    href='javascript:;" onclick="window.open('https://www.69330.net/player/?url='+href,'_blank')"' + ' rel="nofollow';
     $(this).attr('href',href);
 });
+
